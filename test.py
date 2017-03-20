0bd76e57172f8b7b277eab94de82ca51da4fc885
ShangTseTsai <seantsai67@gmail.com>
2017-03-20 12:15:48 +0800

    3/20 note
    bravo

---
+++ test.py
@@ -0,0 +1,12 @@
+print ("hello world");
+import random;
+for i in range(5):
+    print (random.random())
+print("hello world");
+print(2**6);
+print (3);
+
+import sys;
+print (sys.version);
+a = [1, 2, 3];
+print(a[1]);
